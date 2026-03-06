f38a5fc72b211fe609e5ee0c8827e2a7d7df8fa4
FVS-Thiago <feliven@gmail.com>
2026-03-06 18:43:51 -0300

    experimenta of()

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('rxjs-udemy');
+  constructor() {
+    const numbers$ = of([1, 2, 3, 4, 5]);
+
+    numbers$.subscribe((data) => {
+      console.log('observer:', data);
+    });
+  }
 }
