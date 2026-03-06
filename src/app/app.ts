4609df5758153725c0eaf18e8c391cf91723f640
FVS-Thiago <feliven@gmail.com>
2026-03-06 19:11:08 -0300

    testa from() com Promise

--- src/app/app.ts
+++ src/app/app.ts
@@ -19,5 +19,28 @@ export class App {
       console.log('observer:', data);
       this.data.set(data);
     });
+
+    const users = [
+      { id: '1', name: 'John', age: 30 },
+      { id: '2', name: 'Mary', age: 40 },
+      { id: '3', name: 'Igor', age: 20 },
+    ];
+
+    const users$ = of(users);
+
+    users$.subscribe((users) => {
+      console.log(users);
+    });
+
+    const messagePromise = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('Promise resolved');
+      }, 1000);
+    });
+    const message$ = from(messagePromise);
+
+    message$.subscribe((message) => {
+      console.log('promise:', message);
+    });
   }
 }
