6858293d91c40455bd13c8591f571be3f8ab5f64
FVS-MBA <feliven@gmail.com>
2026-03-11 21:48:09 -0300

    experimenta combineLatest()

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { combineLatest, from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,27 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('rxjs-udemy');
+
+  users = [
+    { id: '1', name: 'John', age: 30 },
+    { id: '2', name: 'Mary', age: 40 },
+    { id: '3', name: 'Igor', age: 20 },
+  ];
+
+  messagePromise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Promise resolved');
+    }, 1000);
+  });
+
+  data$ = combineLatest({
+    users: of(this.users),
+    promise: from(this.messagePromise),
+  });
+
+  constructor() {
+    this.data$.subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
