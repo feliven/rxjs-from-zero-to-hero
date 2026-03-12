0dd257aba00ecaf6d1e60742fd60e4b2e45da055
FVS-MBA <feliven@gmail.com>
2026-03-11 22:25:01 -0300

    cria arquivo para tarefa sobre BackendState

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+
 import { Home } from './components/home';
 import { Cursos } from './components/cursos';
 import { Counter } from './components/counter';
 import { Testes } from './components/testes';
+import { BackendState } from './components/backend-state';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
@@ -10,4 +12,5 @@ export const routes: Routes = [
   { path: 'cursos', component: Cursos },
   { path: 'counter', component: Counter },
   { path: 'testes', component: Testes },
+  { path: 'backendstate', component: BackendState },
 ];
---
+++ src/app/components/backend-state.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+
+interface IBackendState {
+  api_url: string;
+  real_views: number;
+  roles: string[];
+}
+
+interface State {
+  apiUrl: string;
+  realViews: number;
+  roles: string[];
+}
+
+@Component({
+  selector: 'app-backend-state',
+  imports: [RouterLink],
+  templateUrl: './backend-state.html',
+})
+export class BackendState {
+  http = inject(HttpClient);
+
+  backendState$: Observable<IBackendState | null> = new Observable((observer) => {
+    observer.next(null);
+
+    setTimeout(() => {
+      observer.next({
+        api_url: 'http://localhost:3004',
+        real_views: 1000,
+        roles: ['admin', 'dev'],
+      });
+    }, 2000);
+  });
+
+  state: State | undefined;
+
+  constructor() {
+    this.backendState$.subscribe((backendState) => {
+      if (!backendState) {
+        return;
+      }
+
+      this.state = {
+        apiUrl: backendState.api_url,
+        realViews: backendState.real_views,
+        roles: backendState.roles,
+      };
+
+      console.log(this.state);
+    });
+  }
+}
