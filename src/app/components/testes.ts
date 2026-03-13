e59355470b3c1096fb37eb34cf07e6d87140d824
FVS-MBA <feliven@gmail.com>
2026-03-13 20:13:21 -0300

    experimenta debounce

--- src/app/components/testes.ts
+++ src/app/components/testes.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { debounceTime } from 'rxjs';
+import { debounce, debounceTime, interval, timer } from 'rxjs';
 
 @Component({
   selector: 'app-testes',
@@ -18,6 +18,12 @@ export class Testes {
     const termoBusca$ = this.buscaForm.get('termoBusca')?.valueChanges;
 
     termoBusca$?.pipe(debounceTime(500)).subscribe((termo) => console.log(termo));
+
+    interval(1000)
+      .pipe(debounce((value) => timer(value * 200)))
+      .subscribe((value) => {
+        console.log('valor:', value, 'tempo de debounce:', value * 200, 'ms');
+      });
   }
 
   onBuscaSubmit() {
