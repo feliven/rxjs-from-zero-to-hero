3e42609c8373350ce6ef1f195a62a58a9b6f4c3b
FVS-MBA <feliven@gmail.com>
2026-03-12 21:40:38 -0300

    experimenta distinctUntilChanged

--- src/app/components/testes.ts
+++ src/app/components/testes.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { distinct, distinctUntilKeyChanged, from } from 'rxjs';
+import { distinctUntilChanged, from } from 'rxjs';
 
 @Component({
   selector: 'app-testes',
@@ -10,16 +10,23 @@ import { distinct, distinctUntilKeyChanged, from } from 'rxjs';
 export class Testes {
   users = [
     { id: '1', name: 'John', age: 30 },
+    { id: '1', name: 'John', age: 30 },
     { id: '2', name: 'Mary', age: 40 },
     { id: '3', name: 'Igor', age: 20 },
-    { id: '4', name: 'Mary', age: 50 },
+    { id: '3', name: 'Igor', age: 20 },
+    { id: '4', name: 'Eve', age: 50 },
+    { id: '3', name: 'Igor', age: 20 },
   ];
 
-  users$ = from(this.users).pipe(distinctUntilKeyChanged('name'));
+  users$ = from(this.users).pipe(
+    distinctUntilChanged((previous, current) => previous.name === current.name),
+  );
 
   constructor() {
     this.users$.subscribe((res) => {
       console.log(res);
     });
+
+    console.log(this.users[0] == this.users[1]);
   }
 }
