ee35ffa6aeae2572d485b2df0416fc1e1a7603a5
FVS-MBA <feliven@gmail.com>
2026-03-14 10:46:21 -0300

    experimenta BehaviorSubject

--- src/app/components/testes.ts
+++ src/app/components/testes.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { User } from '../types';
 
 @Component({
@@ -9,19 +9,23 @@ import { User } from '../types';
   templateUrl: './testes.html',
 })
 export class Testes {
-  subject$ = new Subject<User>();
+  subject$ = new BehaviorSubject<User>({ Id: '0', FirstName: '' });
 
   constructor() {
     this.subject$.subscribe((user) => console.log(user));
 
     this.subject$.next({ Id: '1', FirstName: 'Hello' });
 
+    const valor1 = this.subject$.getValue();
+
+    console.log('valor1', valor1);
+
     setTimeout(() => {
       this.subject$.next({ Id: '2', FirstName: 'World' });
     }, 1000);
 
-    const user$ = this.subject$.asObservable();
+    // const user$ = this.subject$.asObservable();
 
-    user$.subscribe((user) => console.log(user));
+    // user$.subscribe((user) => console.log(user));
   }
 }
